855a2e5c19c9c0976f7f62cce42649afbff5b2fa
Megh-Zyke <ms8615@srmist.edu.in>
2025-11-08 13:54:20 -0500

    initialised CORS

--- server/main.py
+++ server/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.example import router as example_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
 
 @app.get("/")
 async def root():
